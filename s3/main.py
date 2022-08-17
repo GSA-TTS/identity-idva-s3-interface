ea60a9bd50cd79f412ccc592e5daa08507a7c3ca
shumatepf <shumatepfs@gmail.com>
2022-08-17 12:33:43 -0400

    Convert and return file as base64

--- s3/main.py
+++ s3/main.py
@@ -1,24 +1,32 @@
 import os
 from flask import Flask, request, send_file
-from . import s3_interface, settings
-import tempfile
+from . import s3_interface
 
 app = Flask(__name__)
 
+
 @app.route("/list")
 def list():
-    contents = s3_interface.list_files(settings.BUCKET)
+    contents = s3_interface.list_files()
     return str(contents)
 
-@app.route("/get-file", methods=['GET'])
+
+@app.route("/get", methods=['GET'])
 def get_file():
     args = request.args
     file_name = args.get('name')
-    tmp = tempfile.NamedTemporaryFile()
-    image = s3_interface.get_file(file_name, settings.BUCKET, tmp)
-    return send_file(image, mimetype='image/jpeg')
+    image = s3_interface.get_file(file_name)
+    #return send_file(image, mimetype='image/jpeg')
+    return image
+
+
+@app.route("/delete", methods=['DELETE'])
+def delete_file():
+    args = request.args
+    file_name = args.get('name')
+    return s3_interface.delete_file(file_name)
 
 
 if __name__ == "__main__":
-  port = int(os.getenv("PORT", 8080))
-  app.run(host='0.0.0.0', port=port)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
--- s3/s3_interface.py
+++ s3/s3_interface.py
@@ -1,4 +1,6 @@
 import boto3
+import base64
+import tempfile
 from . import settings
 
 s3 = boto3.resource(
@@ -7,23 +9,40 @@ s3 = boto3.resource(
     aws_secret_access_key=settings.SECRET_KEY,
     region_name=settings.REGION
 )
+bucket = s3.Bucket(settings.BUCKET)
 
-# return all images in IDVA S3 bucket
-def list_files(bucket_name):
-    bucket = s3.Bucket(bucket_name)
+
+def list_files():
+    """
+    return all images in IDVA S3 bucket
+    """
     contents = []
     for item in bucket.objects.filter(Prefix="storage/company/"):
         contents.append(item)
 
     return contents
 
-# return specific file from bucket to be stored in temp local file
-def get_file(key, bucket_name, tmp):
-    bucket = s3.Bucket(bucket_name)
-    
+
+def get_file(key):
+    """
+    return specific file in base64 format from bucket to be stored in temp local file
+    """
+    tmp = tempfile.NamedTemporaryFile()
+
+    # save file
     with open(tmp.name, 'wb') as f:
         bucket.download_fileobj(key, f)
-        src = tmp.name
 
-    return src
-    
+    # read and convert file to base64
+    with open(tmp.name, 'rb') as f:
+        b64 = base64.b64encode(f.read())
+
+    return b64.decode('utf-8')
+
+
+def delete_file(key):
+    return bucket.delete_objects(Delete={
+        "Objects": [
+            {"Key": key}
+        ]
+    })
