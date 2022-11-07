679b8e78d20c89a8b215d4558fb04b300e759b53
shumatepf <shumatepfs@gmail.com>
2022-11-07 16:21:48 -0500

    Remove space after import

--- s3/s3_interface.py
+++ s3/s3_interface.py
@@ -2,7 +2,6 @@ import os
 
 from PIL import Image
 
-
 def get_file(key, bucket, tmp):
     """
     return specific file in base64 format from bucket to be stored in temp local file
@@ -22,7 +21,6 @@ def get_file(key, bucket, tmp):
         base_name = os.path.basename(key)
         file_name_no_ext = os.path.splitext(base_name)[0]
         filename = tmp_path + "/" + file_name_no_ext + ".jpeg"
-
 
         image = Image.open(tmp.name)
         image.save(filename)
