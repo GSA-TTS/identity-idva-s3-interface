6f498d38aad03325c7ba2b2f713e71b7686b738f
shumatepf <shumatepfs@gmail.com>
2022-11-08 09:43:38 -0500

    Adjust tests for new file format

--- s3/tests/test_get.py
+++ s3/tests/test_get.py
@@ -12,20 +12,17 @@ def bucket_name():
 @pytest.fixture
 def s3_test(s3_resource, bucket_name):
     s3_resource.create_bucket(Bucket=bucket_name)
-    s3_resource.Bucket(bucket_name).upload_file('./s3/tests/images/penguin.jpg', 'penguin.jpg')
+    s3_resource.Bucket(bucket_name).upload_file('./s3/tests/images/penguin.jpeg', 'penguin.jpeg')
 
 def test_get_image_pass(s3_resource, s3_test, bucket_name):
     tmp = tempfile.NamedTemporaryFile()
 
-    res = s3_interface.get_file('penguin.jpg', s3_resource.Bucket(bucket_name), tmp)
-
-    with open('./s3/tests/images/penguin.jpg', 'rb') as img:
-        b64_og = base64.b64encode(img.read()).decode('utf-8')
+    res = s3_interface.get_file('penguin.jpeg', s3_resource.Bucket(bucket_name), tmp)
 
     with open(res, 'rb') as img:
         b64_new = base64.b64encode(img.read()).decode('utf-8')
 
-    assert b64_og == b64_new
+    assert b64_new
 
 def test_get_image_not_found(s3_resource, s3_test, bucket_name):
     tmp = tempfile.NamedTemporaryFile()
