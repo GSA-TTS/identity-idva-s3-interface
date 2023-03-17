702f26c377636b5c5988203292af6e64006b17af
shumatepf <shumatepfs@gmail.com>
2023-03-17 15:21:47 -0400

    Configure for multiple s3 services

--- s3/main.py
+++ s3/main.py
@@ -1,31 +1,36 @@
-import boto3
 import tempfile
 import mimetypes
 from flask import Flask, jsonify, request, send_file
 from s3 import s3_interface
 from s3 import settings
 
-s3 = boto3.resource(
-    "s3",
-    aws_access_key_id=settings.KEY_ID,
-    aws_secret_access_key=settings.SECRET_KEY,
-    region_name=settings.REGION,
-)
-bucket = s3.Bucket(settings.BUCKET)
+s3_clients = s3_interface.get_clients(settings.S3_CREDENTIALS)
 
 app = Flask(__name__)
 
 
-@app.route("/get", methods=["GET"])
-def get_file():
+@app.route("/get/<vendor_id>/<file_id>", methods=["GET"])
+def get_file(vendor_id, file_id):
     """
     Get file from s3 interface
     """
-    args = request.args
-    file_name = args.get("name")
+
+    if not settings.S3_CREDENTIALS[vendor_id]:
+        return (
+            jsonify(
+                {
+                    "error": "S3 Service not Found",
+                }
+            ),
+            404,
+        )
+
+    bucket = settings.S3_CREDENTIALS[vendor_id]["bucket"]
+    s3_client = s3_clients[vendor_id]
+
     tmp = tempfile.NamedTemporaryFile()
     try:
-        image = s3_interface.get_file(file_name, bucket, tmp)
+        image = s3_interface.get_file(s3_client, file_id, bucket, tmp)
     except FileNotFoundError:
         return (
             jsonify(
@@ -36,7 +41,7 @@ def get_file():
             404,
         )
     else:
-        return send_file(image, mimetype=mimetypes.guess_type(file_name)[0])
+        return send_file(image, mimetype=mimetypes.guess_type(file_id)[0])
 
 
 if __name__ == "__main__":
--- s3/settings.py
+++ s3/settings.py
@@ -11,20 +11,27 @@ log = logging.getLogger(__name__)
 def get_s3_info():
     vcap_services = os.getenv("VCAP_SERVICES")
     try:
-        credentials = json.loads(vcap_services)["s3"][0]["credentials"]
+        service_list = json.loads(vcap_services)["s3"]
     except (json.JSONDecodeError, KeyError) as err:
         log.warning("Unable to load info from VCAP_SERVICES")
         log.debug("Error: %s", str(err))
         raise Exception(str(err))
 
-    key_id = credentials["access_key_id"]
-    secret_key = credentials["secret_access_key"]
-    region = credentials["region"]
-    bucket = credentials["bucket"]
-    return (key_id, secret_key, region, bucket)
+    s3_credentials = {}
+
+    for service in service_list:
+        credentials = service["credentials"]
+        s3_credentials[service["name"]] = {
+            "key_id": credentials["access_key_id"],
+            "secret_key": credentials["secret_access_key"],
+            "region": credentials["region"],
+            "bucket": credentials["bucket"],
+        }
+
+    return s3_credentials
 
 
 PORT = int(os.getenv("PORT", 8080))
 HOST = "0.0.0.0"  # nosec
 
-KEY_ID, SECRET_KEY, REGION, BUCKET = get_s3_info()
+S3_CREDENTIALS = get_s3_info()
