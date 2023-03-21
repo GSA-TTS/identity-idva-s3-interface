4cbd70ab566d6da7e6e7a98596cac9c6f25e8c85
shumatepf <shumatepfs@gmail.com>
2023-03-21 15:52:42 -0400

    Split GET and DELETE

--- s3/main.py
+++ s3/main.py
@@ -9,8 +9,8 @@ s3_clients = s3_interface.get_clients(settings.S3_CREDENTIALS)
 app = Flask(__name__)
 
 
-@app.route("/<service_id>", methods=["GET", "DELETE"])
-def s3_file(service_id):
+@app.route("/<service_id>", methods=["GET"])
+def get_file(service_id):
     """
     Get or Delete file from s3 interface
     """
@@ -32,20 +32,10 @@ def s3_file(service_id):
     file_name = args.get("file")
 
     try:
-        if request.method == "GET":
-            tmp = tempfile.NamedTemporaryFile()
-            image = s3_interface.get_file(s3_client, bucket, file_name, tmp)
-            return send_file(image, mimetype=mimetypes.guess_type(file_name)[0])
-        else:
-            s3_interface.delete_file(s3_client, bucket, file_name)
-            return (
-                jsonify(
-                    {
-                        "success": "True",
-                    }
-                ),
-                200,
-            )
+        tmp = tempfile.NamedTemporaryFile()
+        image = s3_interface.get_file(s3_client, bucket, file_name, tmp)
+        return send_file(image, mimetype=mimetypes.guess_type(file_name)[0])
+
     except FileNotFoundError:
         return (
             jsonify(
@@ -55,6 +45,34 @@ def s3_file(service_id):
             ),
             404,
         )
+
+
+@app.route("/<service_id>", methods=["DELETE"])
+def delete_file(service_id):
+    if not service_id in settings.S3_CREDENTIALS:
+        return (
+            jsonify(
+                {
+                    "error": "S3 Service not Found",
+                }
+            ),
+            404,
+        )
+
+    bucket = settings.S3_CREDENTIALS[service_id]["bucket"]
+    s3_client = s3_clients[service_id]
+
+    args = request.args
+    file_name = args.get("file")
+    s3_interface.delete_file(s3_client, bucket, file_name)
+    return (
+        jsonify(
+            {
+                "success": "True",
+            }
+        ),
+        200,
+    )
 
 
 if __name__ == "__main__":
